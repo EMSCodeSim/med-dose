00295f4be3429cd11bcbd9a88e5f6c6411715239
EMSCodeSim <davidagallaher@hotmail.com>
2026-08-20 20:27:56 -0600

    Build safety-first installable offline PWA

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,7 @@ import Home from "./App";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><Home /></React.StrictMode>);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => navigator.serviceWorker.register("/sw.js").catch(() => undefined));
+}
