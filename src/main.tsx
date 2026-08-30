5253406beb10137c937404e84b9be4e69eaa2183
EMSCodeSim <davidagallaher@hotmail.com>
2026-08-30 15:13:41 -0600

    Load Admin readability fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Home from "./App";
 import "./styles.css";
 import "./unifiedMedicationFlow.css";
+import "./adminReadableFix.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><Home /></React.StrictMode>);
 
