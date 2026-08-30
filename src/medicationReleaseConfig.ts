1e1c63305069c65eba0dda7455180c28ccdbc62f
EMSCodeSim <davidagallaher@hotmail.com>
2026-08-30 11:50:09 -0600

    Centralize released medication defaults

---
+++ src/medicationReleaseConfig.ts
@@ -0,0 +1,20 @@
+export const DEFAULT_FIELD_MEDICATION_IDS = [
+  "adenosine",
+  "amiodarone",
+  "ondansetron",
+  "haloperidol",
+  "atropine",
+  "midazolam",
+  "fentanyl",
+  "calcium",
+  "dextrose",
+  "diphenhydramine",
+  "epinephrine",
+  "magnesium",
+  "methylprednisolone",
+  "naloxone",
+  "sodium-bicarbonate",
+  "ketamine",
+] as const;
+
+export const CURRENT_DMP_PROTOCOL_REVISION = "July 2026";
