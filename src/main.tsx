33ac805b48053c3a0a10b72951eea09761ee0730
EMSCodeSim <davidagallaher@hotmail.com>
2026-09-01 19:00:44 -0600

    Load desktop top visibility fix last

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import "./unifiedMedicationFlow.css";
 import "./adminReadableFix.css";
 import "./activeMedicationHost.css";
+import "./desktopTopVisibilityFix.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><Home /></React.StrictMode>);
 
