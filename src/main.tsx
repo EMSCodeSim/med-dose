a4cb88f69506d60f1ce0217555ef200cadbd9eaf
EMSCodeSim <davidagallaher@hotmail.com>
2026-09-02 15:13:23 -0600

    Load quick-select scrolling fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./adminReadableFix.css";
 import "./activeMedicationHost.css";
 import "./desktopTopVisibilityFix.css";
 import "./approvalStatus.css";
+import "./quickSelectScrollFix.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<React.StrictMode><Home /></React.StrictMode>);
 
